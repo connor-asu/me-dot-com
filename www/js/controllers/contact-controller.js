7e2ea28e36a64ca9f18841217fe0a8d36ff207b6
Connor Eggleston <connor_eggleston@choicehotels.com>
2015-04-05 14:03:24 -0700

    added contact page w/o email function; removed experience page

--- www/js/controllers/contact-controller.js
+++ www/js/controllers/contact-controller.js
@@ -6,10 +6,27 @@
     'use strict';
 
     angular.module("MeDotCom.Controllers")
-        .controller("ContactCtrl", function($scope) {
+        .controller("ContactCtrl", function($scope, $http) {
 
-            $scope.message = "Contact";
-
+            $scope.submitContactRequest = function() {
+                console.dir($scope.message);
+                $http({
+                    method  : 'POST',
+                    url     : 'contact-form.php',
+                    data    : $.param($scope.message),  //param method from jQuery
+                    headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+                }).success(function(data){
+                    console.dir(data);
+                    if (data.success) { //success comes from the return json object
+                        //$scope.submitButtonDisabled = true;
+                        //$scope.resultMessage = data.message;
+                        //$scope.result='bg-success';
+                    } else {
+                        //$scope.submitButtonDisabled = false;
+                        //$scope.resultMessage = data.message;
+                        //$scope.result='bg-danger';
+                    }
+                });
+            };
         });
-
 }(jQuery, angular));
