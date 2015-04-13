08628f4a8ff3a1f4d0dc113f82772a601b971944
Connor Eggleston <connor_eggleston@choicehotels.com>
2015-04-12 19:42:04 -0700

    contact form is functional, version 1 of the site is now live

--- www/contact-form.php
+++ www/contact-form.php
@@ -1,22 +1,71 @@
 <?php 
-	//this page will take an ajax request from the contact form and send you an email
-	$myEmail = "connoreggleston@outlook.com";
+//	//this page will take an ajax request from the contact form and send you an email
+//	$myEmail = "connoreggleston@outlook.com";
+//
+//	//get the data posted by AJAX
+//	$name = $_POST['sender-name'];
+//	$email = $_POST['sender-email'];
+//	$subject = $_POST['subject'];
+//	$content = $_POST['content'];
+//
+//	$message = "Sent by: ".$name."\nEmail: ".$email."\nSubject: ".$subject."\n\n".$content;
+//
+//	//send email
+//	$headers	= "Content-Type: text/plain; charset=iso-8859-1\n";
+//	$headers	.= "From: connoreggleston.com\n";
+//	$recipient = $myEmail;
+//	$subject	= "Personal Website Contact";
+//	$message	= wordwrap($message, 1024);
+//	//send email
+//	mail($recipient, $subject, $message, $headers);
 
-	//get the data posted by AJAX
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$sub = $_POST['subject'];
-	$content = $_POST['message'];
+//**********NEW STUFF******************//
+$errors = array();  // array to hold validation errors
+$data = array();        // array to pass back data
 
-	$message = "Sent by: ".$name."\nEmail: ".$email."\nSubject: ".$sub."\n\n".$content;
+//store data in variables
+$myEmail = "connoreggleston@outlook.com";
+$name = $_POST['sender'];
+$senderEmail = $_POST['senderEmail'];
+$subject = $_POST['subject'];
+$content = $_POST['content'];
 
-	//send email
-	$headers	= "Content-Type: text/plain; charset=iso-8859-1\n";
-	$headers	.= "From: connoreggleston.com\n";
-	$recipient = $myEmail;
-	$subject	= "Personal Website Contact";
-	$message	= wordwrap($message, 1024);
-	//send email
-	mail($recipient, $subject, $message, $headers);
+// validate the variables ========
+if (empty($name))
+    $errors['sender'] = 'Name is required.';
+
+if (empty($senderEmail))
+    $errors['senderEmail'] = 'Email is required.';
+
+if (empty($subject))
+    $errors['subject'] = 'Subject is required.';
+
+if (empty($content))
+    $errors['content'] = 'Message is required.';
+
+// return a response ==============
+
+// response if there are errors
+if (!empty($errors)) { // if there are items in our errors array, return those errors
+    $data['success'] = false;
+    $data['errors']  = $errors;
+} else { // if there are no errors, return success and send the email
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+
+    $message = "Sent by: ".$name."\nEmail: ".$senderEmail."\nSubject: ".$subject."\n\n".$content;
+
+    //send email
+    $headers	= "Content-Type: text/plain; charset=iso-8859-1\n";
+    $headers	.= "From: connoreggleston.com\n";
+    $recipient = $myEmail;
+    $subject	= "Personal Website Contact";
+    $message	= wordwrap($message, 1024);
+    //send email
+    mail($recipient, $subject, $message, $headers);
+}
+
+// return all our data to an AJAX call
+echo json_encode($data);
 
 ?>
--- www/js/controllers/contact-controller.js
+++ www/js/controllers/contact-controller.js
@@ -8,25 +8,40 @@
     angular.module("MeDotCom.Controllers")
         .controller("ContactCtrl", function($scope, $http) {
 
+            //initialize contact variables to false
+            $scope.contactSuccess = false;
+            $scope.contactError = false;
+            $scope.errorMessage = "";
+
             $scope.submitContactRequest = function() {
-                console.dir($scope.message);
+                //initialize contact variables to false
+                $scope.contactSuccess = false;
+                $scope.contactError = false;
+                $scope.errorMessage = "";
                 $http({
                     method  : 'POST',
                     url     : 'contact-form.php',
                     data    : $.param($scope.message),  //param method from jQuery
                     headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
                 }).success(function(data){
-                    console.dir(data);
                     if (data.success) { //success comes from the return json object
-                        //$scope.submitButtonDisabled = true;
-                        //$scope.resultMessage = data.message;
-                        //$scope.result='bg-success';
+                        //show the success message and hide any previous error messages and reset the form
+                        $scope.contactError = false;
+                        $scope.contactSuccess = true;
                     } else {
-                        //$scope.submitButtonDisabled = false;
-                        //$scope.resultMessage = data.message;
-                        //$scope.result='bg-danger';
+                        //extract all of the response error messages and add them to the scope error message
+                        angular.forEach(data.errors, function(value, key) {
+                            if (value) { //there is an error for this field
+                                $scope.errorMessage += value + "\n";
+                            }
+                        });
+
+                        //show the error message and hide any previous success messages
+                        $scope.contactSuccess = false;
+                        $scope.contactError = true;
                     }
                 });
             };
+
         });
 }(jQuery, angular));
